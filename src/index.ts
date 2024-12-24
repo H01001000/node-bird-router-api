228674dccc0b0252552d139f2c58899c2806c8f7
H01001000 <heiheiho000@gmail.com>
2024-12-24 01:42:46 -0800

    style

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,57 @@
-import net from 'net';
+import net from "net";
 
-class Bird {
-  private socket: net.Socket;
-  private socketPath: string;
+export class Bird {
+  private readonly socket: net.Socket;
+  private readonly socketPath: string;
 
   private connected: boolean = false;
 
   /**
-  * Bird socket connection.
-  * @constructor
-  * @param {Object} options - 
-  * @param {string} [options.socketPath="/run/bird/bird.ctl"] - Path to the bird socket. Default is `/run/bird/bird.ctl`
-  */
+   * Bird socket connection.
+   * @constructor
+   * @param {Object} options -
+   * @param {string} [options.socketPath="/run/bird/bird.ctl"] - Path to the bird socket. Default is `/run/bird/bird.ctl`
+   */
   constructor(options?: { socketPath?: string }) {
-
     const defaultOptions = {
-      socketPath: "/run/bird/bird.ctl"
-    }
+      socketPath: "/run/bird/bird.ctl",
+    };
 
-    this.socketPath = options?.socketPath || defaultOptions.socketPath;
+    this.socketPath = options?.socketPath ?? defaultOptions.socketPath;
+    this.socket = new net.Socket();
   }
 
-  async connect() {
+  async connect(): Promise<boolean> {
+    this.socket.connect(this.socketPath);
 
-    this.socket = net.createConnection(this.socketPath);
-
-    return new Promise((resolve, reject) => {
+    return await new Promise((resolve, reject) => {
       this.socket.once("data", (data) => {
         if (!data.toString().match(/BIRD [0-9]+\.[0-9]+ ready./)) {
           reject(new Error("Failed to connect to bird socket"));
         }
         this.connected = true;
         resolve(true);
-      })
+      });
     });
   }
 
-  async sendCommand(command: string) {
+  async sendCommand(command: string): Promise<string> {
     if (!this.connected) {
       throw new Error("Socket is not connected");
     }
 
-    const reposonse = new Promise((resolve) => {
-      this.socket.once('data', (data) => {
-        resolve(data.toString().replaceAll(/([0-9]{4}(-|\s)?)|(^\s+)/gm, "").replaceAll(/  +/g, " "));
+    const reposonse = new Promise<string>((resolve) => {
+      this.socket.once("data", (data) => {
+        resolve(
+          data
+            .toString()
+            .replaceAll(/([0-9]{4}(-|\s)?)|(^\s+)/gm, "")
+            .replaceAll(/  +/g, " "),
+        );
       });
-    })
+    });
 
     this.socket.write(command);
-    return reposonse;
+    return await reposonse;
   }
 }
-
